4109330d6898d951ca1b8143163bf7c1f433fcac
Doug Franklin <doug@dougfranklin.com>
2023-05-21 19:17:41 -0400

    Search input target values acquired

--- scripts.js
+++ scripts.js
@@ -12,15 +12,16 @@ const items = [
   { id: "010", name: "Tennis Ball", price: 100 },
 ];
 
+// Shopping Functions //////////////////////
 const item = document.getElementById('item')
 const prev = document.getElementById('prev')
 const next = document.getElementById('next')
 
-let index = 0
-
 const shopForItem = n => {
   item.innerHTML = items[n].name
 }
+
+let index = 0 // This is also used in the showItem function, see line 38
 
 shopForItem(index)
 next.addEventListener('click', () => {
@@ -32,19 +33,19 @@ prev.addEventListener('click', () => {
   index = (index +  items.length - 1) % items.length 
   shopForItem(index)
 })
+// End of Shopping Functions //////////////////////
 
-const boxButton = document.getElementById("addBoxButton");
-// boxButton.addEventListener("click", myFunction);
-// boxButton.addEventListener("click", () => showItem(items[0]));
-// boxButton.addEventListener("click", () => loopItems);
+// Add Item Functions /////////////////////////////////////
+
+const selectedItems = []; // These appear as selections for searching
+
+const boxButton = document.getElementById("addItemButton");
 boxButton.addEventListener("click", () => showItem(index));
 
 let boxContainer = document.getElementById("box-container");
 boxContainer.classList.add("box-container");
 
-// https://stackoverflow.com/questions/74459851/how-to-iterate-through-an-array-on-each-click-of-an-event-listener
 function showItem(n) {
-
   let item = items[n];
 
   const itemsContainer = document.getElementById("items-container");
@@ -61,34 +62,18 @@ function showItem(n) {
   <div class="item-price">${item.price}</div>
   <div class="item-id">${item.id}</div>
   `;
+
   itemsContainer.appendChild(itemDiv);
+
+  selectedItems.push(item);
 }
+// End of Add Item Functions //////////////////////
 
-// function showItems() {
-//   const itemsContainer = document.getElementById("items-container");
-//   itemsContainer.classList.add("items-container");
+// Search Functions //////////////////////////////
+const searchInput = document.getElementById("searchInput");
 
-//   items.forEach((item) => {
-//     const itemDiv = document.createElement("div");
-//     itemDiv.classList.add("item");
-//     itemDiv.innerHTML = `
-//     <div class="item-name">${item.name}</div>
-//     <div class="item-price">${item.price}</div>
-//     <div class="item-id">${item.id}</div>
-//     `;
-//     itemsContainer.appendChild(itemDiv);
-//   });
-// }
+searchInput.addEventListener("keyup", handleSearch);
 
-function myFunction() {
-
-  console.log("Button clicked");
-
-  let repeatableBox = document.createElement("div"); // new box
-  repeatableBox.classList.add("myDiv");
-
-  let textNode = document.createTextNode("A");
-
-  boxContainer.appendChild(repeatableBox);
-  repeatableBox.appendChild(textNode);
+function handleSearch(e) {
+  console.log(e.target.value);
 }
