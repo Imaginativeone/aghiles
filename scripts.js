bad1fdbee8f3a57f3cebcbacfb59945d94827e14
Doug Franklin <doug@dougfranklin.com>
2023-05-21 15:45:15 -0400

    Next, figure out CSS expandability

--- scripts.js
+++ scripts.js
@@ -14,6 +14,14 @@ function myFunction() {
   let repeatableBox = document.createElement("div"); // new box
   repeatableBox.classList.add("myDiv");
 
+  let textNode = document.createTextNode("A");
+
+  let someContainer = document.getElementById("box-container");
+
   // document.body.appendChild(box);
   boxContainer.appendChild(repeatableBox);
+  repeatableBox.appendChild(textNode);
+
+  // someContainer.appendChild(textNode);
+  someContainer.appendChild(repeatableBox);
 }
